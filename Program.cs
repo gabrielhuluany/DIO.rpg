ade83231a5964972c5d026de426c43e7a8f661c2
gabrielhuluany <gabrielhuluany@outlook.com>
2022-01-29 05:06:02 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.rpg.Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guerreiro Gus = new Guerreiro();
+            Mago JoJo = new Mago();
+            Arqueiro Lamps = new Arqueiro();
+        }
+    }
+}
