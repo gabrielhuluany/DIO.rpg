f984ff4279038b0cfe6592cf4a33551be9701ac1
gabrielhuluany <gabrielhuluany@outlook.com>
2022-01-29 04:48:50 -0300

    Start!

---
+++ Entities/Personagem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.rpg.Entities
+{
+    public abstract class Personagem
+    {
+        public string nome;
+        public int nivel;
+        public int vida;
+        public int força;
+    }
+}
